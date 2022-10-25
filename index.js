8861331e2a77964acc21a61b073f78e61aa4e908
laboneeaktar <labonipub@gmail.com>
2022-10-25 10:31:41 +0600

    send single course data

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require("./data/courses.json");
+
 app.get("/", (req, res) => {
   res.send("Learn Coding API is Running");
+});
+
+//get all courses data
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+//get single courses data
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.find((c) => c.id === id);
+  res.send(course);
 });
 
 app.listen(port, () => {
